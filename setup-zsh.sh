1d8b6e9c3876bccb56e8f6e35e6f7f62446139cd
Paladin4ick <130995344+Paladin4ick@users.noreply.github.com>
2025-09-24 20:44:53 +0600

    Update setup-zsh.sh

--- setup-zsh.sh
+++ setup-zsh.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -e
 
 sudo apt update && sudo apt install -y zsh git curl
@@ -9,19 +8,20 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 fi
 
 ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
 if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
     $ZSH_CUSTOM/themes/powerlevel10k
 fi
 
+if [ ! -d "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting" ]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+    $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+fi
+
 ZSHRC="$HOME/.zshrc"
-
 sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$ZSHRC"
-
-sed -i 's/^plugins=.*/plugins=(git docker docker-compose python ssh vscode eza)/' "$ZSHRC"
-
-sed -i '/^# Example aliases/Q' "$ZSHRC"
+sed -i 's/^plugins=.*/plugins=(git docker docker-compose python ssh vscode eza zsh-syntax-highlighting)/' "$ZSHRC"
 
 chsh -s $(which zsh)
-
-echo "✅ Установка завершена. Перезапусти терминал или выполни 'exec zsh'"
+echo "Установка завершена. Перезапусти терминал или выполни 'exec zsh'"
