cba9579e693ef7fae76741b7120aadd91e0c415b
Paladin4ick <130995344+Paladin4ick@users.noreply.github.com>
2025-09-24 20:40:29 +0600

    Add script

---
+++ setup-zsh.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+sudo apt update && sudo apt install -y zsh git curl
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+if [ ! -d "$ZSH_CUSTOM/themes/powerlevel10k" ]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git \
+    $ZSH_CUSTOM/themes/powerlevel10k
+fi
+
+ZSHRC="$HOME/.zshrc"
+
+sed -i 's/^ZSH_THEME=.*/ZSH_THEME="powerlevel10k\/powerlevel10k"/' "$ZSHRC"
+
+sed -i 's/^plugins=.*/plugins=(git docker docker-compose python ssh vscode eza)/' "$ZSHRC"
+
+sed -i '/^# Example aliases/Q' "$ZSHRC"
+
+chsh -s $(which zsh)
+
+echo "✅ Установка завершена. Перезапусти терминал или выполни 'exec zsh'"
