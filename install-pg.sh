0cea8e50c2cd131d74d40aab5c0ec6c0890c2266
Paladin4ick <130995344+Paladin4ick@users.noreply.github.com>
2025-09-26 14:35:49 +0600

    Update install-pg.sh

--- install-pg.sh
+++ install-pg.sh
@@ -13,9 +13,10 @@ sudo systemctl enable postgresql
 sudo systemctl start postgresql
 
 echo "Добавление репозитория pgAdmin..."
-curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo tee /etc/apt/trusted.gpg.d/pgadmin.gpg > /dev/null
+sudo curl -fsSL https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/pgadmin.gpg
 
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list'
+
 
 sudo apt update
 
